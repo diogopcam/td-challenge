95ed94a176e76439c52490ad1e958c85a662dd12
Diogo Pessin Ca <diogpcam@Diogo-Camargo---MacBook-Pro.local>
2025-12-02 14:24:20 -0300

    Exclusao de arquivos inuteis.

--- camera-poc/3D/Controls/ButtonManager.swift
+++ camera-poc/3D/Controls/ButtonManager.swift
@@ -5,7 +5,6 @@
 //  Created by Diogo Camargo on 02/12/25.
 //
 
-
 class ButtonManager {
     static let shared = ButtonManager()
     private init() {}
@@ -14,7 +13,6 @@ class ButtonManager {
 
     func disable() {
         isEnabled = false
-        SoundManager.shared.playSound(named: "blocked") // opcional
     }
 
     func enable() {
